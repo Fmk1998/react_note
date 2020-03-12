524dbc3f86c1ffd89e9110d630bfe527c259e7d8
fmk15387033268 <1092911991@qq.com>
2020-03-12 13:02:14 +0800

    react订阅消息发布

--- src-users/components/app/app.jsx
+++ src-users/components/app/app.jsx
@@ -4,12 +4,21 @@ import Main from "../main/main";
 
 export default class App extends Component {
 
+    state = {
+        searchName: ''
+    }
+
+    setSearchName = (searchName) => {
+        //更新状态
+        this.setState({searchName})
+    }
+
     render() {
 
         return (
             <div className="container">
-                <Search/>
-                <Main/>
+                <Search setSearchName={this.setSearchName}/>
+                <Main searchName={this.state.searchName}/>
             </div>
         )
     }
